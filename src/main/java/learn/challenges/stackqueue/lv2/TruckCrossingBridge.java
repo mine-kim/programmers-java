a22a9169a5a06c843bea57a1bda3b740e0248a6b
kmh <minhye0619@gmail.com>
2024-11-03 21:53:57 +0900

    다리를 지나는 트럭 문제 풀이

---
+++ src/main/java/learn/challenges/stackqueue/lv2/TruckCrossingBridge.java
@@ -0,0 +1,9 @@
+package learn.challenges.stackqueue.lv2;/**
+ * ======================================================
+ * packageName : learn.challenges.stackqueue.lv2
+ * fileName    : TruckCrossingBridge
+ * date        : 11/3/24
+ * description :
+ * ======================================================
+ */public class TruckCrossingBridge {
+}
